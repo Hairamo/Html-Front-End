5d75ffc0aa9268af13ac6b47417879f6ff1d604d
Hairamo <105130822+Hairamo@users.noreply.github.com>
2026-06-23 21:41:47 -0300

    Update script.js

--- HTML-js/ex009-exercicio/script.js
+++ HTML-js/ex009-exercicio/script.js
@@ -8,6 +8,7 @@ function verificar(){
         resul.innerHTML = `<p>Sua velocidade atual é ${nome} KM/h</p> <p> Velocidade esta dentro do limite permitido.</p><p> Use sempre cinto de segurança</p>`
     }else {
         resul.innerHTML = `<p>Sua velocidade atual é ${nome} KM/h</p> <p> Você esta <span id='red'>multado</span> por excesso de velocidade.</p><p> Use sempre cinto de segurança</p>`
-        
+        let red = document.getElementById(`red`)
+        red.style.color = 'red'
     }
 }
