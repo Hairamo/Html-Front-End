a5a8a5d80e2b0f953f7274ad9eb3f7024ba15000
Hairamo <105130822+Hairamo@users.noreply.github.com>
2026-06-29 19:08:31 -0300

    atividade tentativa

--- HTML-js/ex011-atividade/script.js
+++ HTML-js/ex011-atividade/script.js
@@ -1,13 +1,18 @@
 function cadastro(){
-
+    let nomes = document.getElementById(`list`).value
+    resul = nomes
+    
 }
 
 function imprimir(){
-
+    let resul = document.getElementById(`resultado`)
+    for (var n in nomes) {
+        resul.innerHTML = `Usuários = ${nomes}`
+    }
 
 }
 
 function limpar(){
-
+    resul.innerHTML = ''
     
 }
